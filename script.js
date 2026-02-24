9b05c3d2fdc9a91872bf9d37d758d55f3699cc43
JD Robison <jdrobison00@gmail.com>
2026-02-24 14:54:49 -0500

    Create read toggle functionality.

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+} 
+
+function pressToggle(event, book) {
+    const readSpan = event.target.parentElement;
+
+    //change object's status
+    book.toggleRead();
+
+    //change display
+    const read = readSpan.querySelector("p");
+    if(book.read === true) {
+        read.textContent = "Already read";
+    } else {
+        read.textContent = "Not read yet"
+    }
+}
+
 function deleteBook(event, book) {
     const bookCard = event.target.parentElement;
     
@@ -23,30 +42,49 @@ function deleteBook(event, book) {
 function addBookToLibrary(title, author, pages, read, library) {
     let book = new Book(title, author, pages, read);
     library.push(book);
+    return book;
 }
 
 function displayBook(book) {
+    // create card element
     let card = document.createElement("div");
     card.classList.add("card");
+
+    //title+author line
     let titleAuthor = document.createElement("h2");
     titleAuthor.textContent = `${book.title} by ${book.author}`;
     card.appendChild(titleAuthor);
+
+    //num pages line
     let pages = document.createElement("p");
     pages.textContent = `Pages: ${book.pages}`;
     card.appendChild(pages);
+
+    //read or unread
+    let readSpan = document.createElement("span");
+    readSpan.classList.add("read-line")
     let read = document.createElement("p")
     if(book.read === false) {
         read.textContent = "Not read yet";
     } else {
         read.textContent = "Already read";
     }
-    card.appendChild(read);
+    readSpan.appendChild(read);
+    let readButton = document.createElement("button");
+    readButton.classList.add("toggle");
+    readButton.textContent = "Toggle";
+    readButton.addEventListener("click", event => pressToggle(event, book));
+    readSpan.appendChild(readButton);
+    card.appendChild(readSpan);
+
+    //delete button
     let delButton = document.createElement("button");
     delButton.classList.add("delete");
     delButton.textContent = "Delete";
     delButton.addEventListener("click", event => deleteBook(event, book));
     card.appendChild(delButton);
 
+    //add card to display
     display.appendChild(card);
 }
 
@@ -65,9 +103,7 @@ function submitForm(event, form) {
     const pages = formData.get("pages");
     const read = formData.get("read");
 
-    addBookToLibrary(title, author, pages, read, myLibrary);
-
-    const newBook = new Book(title, author, pages, read);
+    const newBook = addBookToLibrary(title, author, pages, read, myLibrary);
     displayBook(newBook);
     body.removeChild(form);
 }
@@ -175,8 +211,5 @@ function addBook(event) {
 }
 
 newBook.addEventListener("click", event => addBook(event));
-
-addBookToLibrary("Braiding Sweetgrass", "Robin Wall Kimmerer", 350, true, myLibrary);
-addBookToLibrary("The Odyssey", "Homer", 500, false, myLibrary);
 
 displayLibrary(myLibrary);
